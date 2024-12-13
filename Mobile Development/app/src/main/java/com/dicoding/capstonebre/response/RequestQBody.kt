e9101259b33b364fea997cab5f325ea167aa08fb
Guusstt <muhammadgustikaa@gmail.com>
2024-12-13 13:52:35 +0700

    2nd commit

--- Mobile Development/app/src/main/java/com/dicoding/capstonebre/response/RequestQBody.kt
+++
@@ -1,9 +0,0 @@
-package com.dicoding.capstonebre.response
-
-import com.google.gson.annotations.SerializedName
-
-data class RequestQBody(
-
-	@field:SerializedName("answer")
-	val answer: String? = null
-)
--- Mobile Development/app/src/main/java/com/dicoding/capstonebre/ui/welcome/WelcomeViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.dicoding.capstonebre.ui.welcome
-
-
-
-import androidx.lifecycle.ViewModel
-
-class WelcomeViewModel : ViewModel() {
-    // Tambahkan logika tambahan di sini jika diperlukan
-}
